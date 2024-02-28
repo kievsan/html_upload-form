c22e49dad4a7139b0af12e28bf4fe2153f7103c2
kievskiy <knhel7@mail.ru>
2024-02-28 06:54:09 +0300

    upload: if upload dir not found

--- upload.php
+++ upload.php
@@ -5,11 +5,16 @@ $uploaddir = './upload/';
 $filename = $_POST['file_name'];
 $uploadfile = $uploaddir . basename($filename);
 
-if ($filename && move_uploaded_file($_FILES['content']['tmp_name'], $uploadfile)) {
+if ($filename && makeDir($uploaddir) && move_uploaded_file($_FILES['content']['tmp_name'], $uploadfile)) {
     echo 'Информация:';
     echo '<pre>';
-    echo 'Загружен файл '. $uploadfile . ' размером ' . $_FILES['content']['size'] . ' байт.';
+    echo 'Загружен файл ' . $uploadfile . ' размером ' . $_FILES['content']['size'] . ' байт.';
     echo '<pre>';
 } else {
     header('location: upload_form.html');
+}
+
+function makeDir($path)
+{
+     return is_dir($path) || mkdir($path);
 }
