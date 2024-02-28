b6b3704dbff0209825201727dfa09c97d006e440
kievskiy <knhel7@mail.ru>
2024-02-28 04:23:52 +0300

    Init. task2

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+// Домашнее задание к занятию «3.2. HTML-форма»
+// Задание 2: обрабатываем HTML-форму
+// https://github.com/netology-code/bphp-2-homeworks/tree/master/007-forms
+
+
+header('location: upload_form.html');
